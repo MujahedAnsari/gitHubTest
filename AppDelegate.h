1b604b39e0f10da1e4bfa7b75edf4287e1414255
MujahedAnsari <email2mujahed@gmail.com>
2016-07-22 17:34:58 +0530

    aapD

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AnalyticsExample
+//
+//  Created by Netbiz Systems on 09/02/16.
+//  Copyright © 2016 com.google.samples.quickstart.AnalyticsExample. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
